d679af23d4b73a8ad0fe549538b420f076af2010
Martin-Mtz <imartinez@iwa.com.mx>
2024-11-06 12:17:38 -0600

    Martin-calculator-with-interface
    Martin-calculator-with-interface

---
+++ src/src/main/java/com/iwaconsolti/calculator02/IOperations.java
@@ -0,0 +1,6 @@
+package com.mx.iwaconsolti.calculator02;
+
+public interface IOperations{
+
+    double operation(double num1, double num2);
+}
---
+++ src/src/main/java/com/iwaconsolti/calculator02/SubtractionService.java
@@ -0,0 +1,12 @@
+package com.mx.iwaconsolti.calculator02;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubtractionService implements IOperations{
+
+    @Override
+    public double operation(double num1, double num2) {
+        return num1 - num2;
+    }
+}
